b80bf613beb7a13130fb2f0ae351e81adb4421c0
Alexander Ilseman <al@ilseman.com>
2024-05-17 00:44:41 -0500

    WIP: draft ball colision logic with bug

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use std::f64::consts::PI;
+
 use glutin_window::GlutinWindow as Window;
 use graphics::math as graph_math;
 use graphics::math::Vec2d;
@@ -25,6 +27,7 @@ struct Ball {
     position: Vec2d,
     velocity: Vec2d,
     radius: f64,
+    mass: f64,
 }
 
 impl Ball {
@@ -41,10 +44,8 @@ impl Ball {
             }
         }
     }
-    fn handle_ball_colisions(&mut self, inital_ball_props: &Vec<Ball>, self_index: usize) {
-        // TODO: That's right, the logic for handling these is more complex than
-        // for the boundaries. The borrow checker seems designed to disallow
-        // the way I implemented this in python
+    fn handle_ball_colisions(&mut self, inital_ball_props: &[Ball], self_index: usize) {
+        let damping = 1.0;
         for (i, other) in inital_ball_props.iter().enumerate() {
             if i == self_index {
                 break;
@@ -58,18 +59,43 @@ impl Ball {
             let scale = 1.0 / diff_len;
             let normalized_direction = diff.map(|d| d * scale);
             let correction_scaler = (center_seperation_len - diff_len) / 2.0;
-            self.position = graph_math::scale(
-                graph_math::add(self.position, normalized_direction),
-                correction_scaler,
-            )
+            let pos_n_dir_sum = graph_math::add(self.position, normalized_direction);
+            self.position = [
+                pos_n_dir_sum[0] * -correction_scaler,
+                pos_n_dir_sum[1] * -correction_scaler,
+            ];
+
+            let self_init_v = graph_math::dot(self.velocity, normalized_direction);
+            let other_init_v = graph_math::dot(other.velocity, normalized_direction);
+
+            let m1 = self.mass;
+            let m2 = other.mass;
+            let combined_mass = m1 + m2;
+
+            let self_end_v = (m1 * self_init_v + m2 * other_init_v
+                - m2 * (self_init_v - other_init_v) * damping)
+                / combined_mass;
+
+            // INFO: Pretty sure this and the line begining let other_diff_v...
+            // won't be utilized the way they were in my Python implementation
+            // but I'm going to keep them here untill I'm clear on the best way
+            // to optimize this approach
+
+            // let other_end_v = (m1 * self_init_v + m2 * other_init_v
+            //     - m1 * (other_init_v - self_init_v) * damping)
+            //     / combined_mass;
+            // let other_diff_v = other_end_v - other_init_v;
+
+            let self_diff_v = self_end_v - self_init_v;
+            let sum_v_and_normal_direction = graph_math::add(self.velocity, normalized_direction);
+            self.velocity = [
+                sum_v_and_normal_direction[0] * self_diff_v,
+                sum_v_and_normal_direction[1] * self_diff_v,
+            ];
         }
     }
 }
 
-/*
-    render: Draw a visualization of the simulation
-    update: Update the parameters of the simulation
-*/
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
@@ -102,6 +128,10 @@ impl App {
     }
 }
 
+fn radius_to_volume_in_l3(radius: f64) -> f64 {
+    (4.0 / 3.0) * PI * radius.powf(3.0)
+}
+
 fn main() {
     // let opengl = OpenGL::V2_1;
     let opengl = OpenGL::V3_2;
@@ -115,14 +145,25 @@ fn main() {
 
     // let mut rng = rand::thread_rng();
     // Create new game and run
+    let radius = 50.0;
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        bodies: vec![Ball {
-            position: [100.0, 100.0],
-            // velocity: [rng.gen::<f64>() * 10.0, 0.0],
-            velocity: [60.0, 50.0],
-            radius: 10.0,
-        }],
+        bodies: vec![
+            Ball {
+                position: [140.0, 200.0],
+                // velocity: [rng.gen::<f64>() * 10.0, 0.0],
+                velocity: [120.0, 10.0],
+                radius,
+                mass: radius_to_volume_in_l3(radius),
+            },
+            Ball {
+                position: [100.0, 100.0],
+                // velocity: [rng.gen::<f64>() * 10.0, 0.0],
+                velocity: [60.0, 50.0],
+                radius,
+                mass: radius_to_volume_in_l3(radius),
+            },
+        ],
     };
 
     let mut events = Events::new(EventSettings::new());

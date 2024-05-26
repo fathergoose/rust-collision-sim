7c13f04cbb5768926550e958b26b007250f6754e
Alexander Ilseman <al@ilseman.com>
2024-05-26 18:28:53 -0500

    Incremental-comp change

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use graphics::math::Vec2d;
 use graphics::{Line, Polygon};
 /// A 2D vector.
 //pub type Vector2<T> = [T; 2];
+
 struct App {}
 struct Window {}
 struct Simulation {}
@@ -16,9 +17,9 @@ struct Enclosure {
     walls: [[i64; 2]; 2],
 }
 struct Particle {
-    // particles are spheres for now
+    // spheres for now
     radius: u64,
-    mass: u64, // May i define this in terms of its radius?
+    mass: u64,
     position: Vec2d<i64>,
     velocity: Vec2d<i64>,
 }

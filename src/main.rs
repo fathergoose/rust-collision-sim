457d8b055d363980f50f6178185f3c704988d64a
Alexander Ilseman <al@ilseman.com>
2024-05-26 18:41:52 -0500

    WIP: copying original implementation

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,23 @@
 // out the rough outline of an application.
 
 extern crate graphics;
+use glutin_window::OpenGL;
 use graphics::math::Vec2d;
 use graphics::{Line, Polygon};
+use opengl_graphics::GlGraphics;
+use piston::{Window, WindowSettings};
 /// A 2D vector.
 //pub type Vector2<T> = [T; 2];
 
-struct App {}
-struct Window {}
-struct Simulation {}
-struct SimulationView {}
+struct App {
+    gl: GlGraphics,
+}
+// Handled by glutin window?
+// struct Window {}
+struct Simulation {
+    bodies: Vec<Particle>,
+}
+struct View {}
 struct Enclosure {
     walls: [[i64; 2]; 2],
 }
@@ -24,4 +32,13 @@ struct Particle {
     velocity: Vec2d<i64>,
 }
 
-fn main() {}
+fn main() {
+    let opengl = OpenGL::V3_2;
+
+    // Create Glutin window.
+    let mut window: Window = WindowSettings::new("bouncing-balls", [X_MAX, Y_MAX])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+}

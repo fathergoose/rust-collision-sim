61160bfbb390f2dc876543a719825d2134939bd3
Alexander Ilseman <al@ilseman.com>
2024-06-14 05:48:55 -0500

    Possibly working

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-// use rand::prelude::*;
 
 const X_MAX: u32 = 500;
 const Y_MAX: u32 = 500;
+const N_BODY: usize = 2;
 
 pub struct App {
     gl: GlGraphics,
-    bodies: Vec<Ball>,
+    bodies: [Ball; N_BODY],
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Ball {
-    position: Vec2d,
-    velocity: Vec2d,
+    position: Vec2d<f64>,
+    velocity: Vec2d<f64>,
     radius: f64,
     mass: f64,
 }
@@ -37,29 +37,28 @@ impl Ball {
             self.position[1] - self.radius,
         ]
     }
-    fn handle_boundary_colision(&mut self, boundries: Vec2d) {
+    fn handle_boundary_colision(&self, boundries: Vec2d) -> Ball {
         for (i, item) in boundries.iter().enumerate() {
             if self.position[i] >= item - self.radius || self.position[i] <= self.radius {
-                self.velocity[i] = -self.velocity[i]
+                let mut new_ball = *self;
+                new_ball.velocity[i] = -self.velocity[i];
+                return new_ball;
             }
         }
+        *self
     }
-    fn handle_ball_colisions(&mut self, other: &mut Ball) {
+    fn handle_ball_colisions(&self, other: &Ball) -> Option<(Ball, Ball)> {
         let damping = 1.0;
         let diff = graph_math::sub(self.position, other.position);
         let diff_len = graph_math::square_len(diff).sqrt();
         let center_seperation_len = self.radius + other.radius;
         if diff_len == 0.0 || diff_len > center_seperation_len {
-            return;
+            return None;
         }
         let scale = 1.0 / diff_len;
         let normalized_direction = diff.map(|d| d * scale);
         let correction_scaler = (center_seperation_len - diff_len) / 2.0;
         let pos_n_dir_sum = graph_math::add(self.position, normalized_direction);
-        self.position = [
-            pos_n_dir_sum[0] * -correction_scaler,
-            pos_n_dir_sum[1] * -correction_scaler,
-        ];
 
         let self_init_v = graph_math::dot(self.velocity, normalized_direction);
         let other_init_v = graph_math::dot(other.velocity, normalized_direction);
@@ -71,23 +70,41 @@ impl Ball {
         let self_end_v = (m1 * self_init_v + m2 * other_init_v
             - m2 * (self_init_v - other_init_v) * damping)
             / combined_mass;
-
-        // INFO: Pretty sure this and the line begining let other_diff_v...
-        // won't be utilized the way they were in my Python implementation
-        // but I'm going to keep them here untill I'm clear on the best way
-        // to optimize this approach
-
-        // let other_end_v = (m1 * self_init_v + m2 * other_init_v
-        //     - m1 * (other_init_v - self_init_v) * damping)
-        //     / combined_mass;
-        // let other_diff_v = other_end_v - other_init_v;
-
         let self_diff_v = self_end_v - self_init_v;
         let sum_v_and_normal_direction = graph_math::add(self.velocity, normalized_direction);
-        self.velocity = [
-            sum_v_and_normal_direction[0] * self_diff_v,
-            sum_v_and_normal_direction[1] * self_diff_v,
-        ];
+
+        let other_end_v = (m1 * self_init_v + m2 * other_init_v
+            - m1 * (other_init_v - self_init_v) * damping)
+            / combined_mass;
+        let other_diff_v = other_end_v - other_init_v;
+        let other_sum_v_and_normal_direction =
+            graph_math::add(other.velocity, normalized_direction);
+
+        let new_self = Ball {
+            position: [
+                pos_n_dir_sum[0] * -correction_scaler,
+                pos_n_dir_sum[1] * -correction_scaler,
+            ],
+            velocity: [
+                sum_v_and_normal_direction[0] * self_diff_v,
+                sum_v_and_normal_direction[1] * self_diff_v,
+            ],
+            mass: self.mass,
+            radius: self.radius,
+        };
+        let new_other = Ball {
+            position: [
+                pos_n_dir_sum[0] * correction_scaler,
+                pos_n_dir_sum[1] * correction_scaler,
+            ],
+            velocity: [
+                other_sum_v_and_normal_direction[0] * other_diff_v,
+                other_sum_v_and_normal_direction[1] * other_diff_v,
+            ],
+            mass: other.mass,
+            radius: other.radius,
+        };
+        Some((new_self, new_other))
     }
 }
 
@@ -112,17 +129,44 @@ impl App {
     fn update(&mut self, args: &UpdateArgs) {
         let surface = [Into::<f64>::into(X_MAX), Into::<f64>::into(Y_MAX)];
 
-        let mut init_ball_states = self.bodies.clone();
+        let init_ball_states = self.bodies;
+        let mut result_ball_states: [Ball; 2] = [Ball {
+            position: [0.0, 0.0],
+            velocity: [0.0, 0.0],
+            radius: 0.0,
+            mass: 0.0,
+        }; 2];
 
-        for (i, b) in self.bodies.iter_mut().enumerate() {
-            b.position[0] += args.dt * b.velocity[0];
-            b.position[1] += args.dt * b.velocity[1];
+        for (i, b) in self.bodies.iter().enumerate() {
+            /*
+             * I need to re-think the whole thing with no mutability
+             * At this point, I've avoided it everywhere but `fn check_boundary_colision(...)`
+             * There's really no need for it if one doesn't care about performance (I don't)
+             */
+
+            tick(b, args);
             b.handle_boundary_colision(surface);
-            for ob in init_ball_states.iter_mut().skip(i + 1) {
-                b.handle_ball_colisions(ob);
+            for (j, ob) in init_ball_states.iter().skip(i + 1).enumerate() {
+                match b.handle_ball_colisions(ob) {
+                    Some((ball, other_ball)) => {
+                        result_ball_states[i] = ball;
+                        result_ball_states[j] = other_ball
+                    }
+                    None => {
+                        result_ball_states[i] = *b;
+                        result_ball_states[j] = *ob;
+                    }
+                }
             }
         }
     }
+}
+
+fn tick(b: &Ball, args: &UpdateArgs) -> Vec2d<f64> {
+    [
+        b.position[0] + args.dt * b.velocity[0],
+        b.position[1] + args.dt * b.velocity[1],
+    ]
 }
 
 fn radius_to_volume_in_l3(radius: f64) -> f64 {
@@ -130,32 +174,26 @@ fn radius_to_volume_in_l3(radius: f64) -> f64 {
 }
 
 fn main() {
-    // let opengl = OpenGL::V2_1;
     let opengl = OpenGL::V3_2;
 
-    // Create Glutin window.
     let mut window: Window = WindowSettings::new("bouncing-balls", [X_MAX, Y_MAX])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    // let mut rng = rand::thread_rng();
-    // Create new game and run
     let radius = 50.0;
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        bodies: vec![
+        bodies: [
             Ball {
                 position: [140.0, 200.0],
-                // velocity: [rng.gen::<f64>() * 10.0, 0.0],
                 velocity: [120.0, 10.0],
                 radius,
                 mass: radius_to_volume_in_l3(radius),
             },
             Ball {
                 position: [100.0, 100.0],
-                // velocity: [rng.gen::<f64>() * 10.0, 0.0],
                 velocity: [60.0, 50.0],
                 radius,
                 mass: radius_to_volume_in_l3(radius),
